86ece4feb11c002445ebfa4de40be7e3f0102c8c
stanminister <stians_egen@hotmail.com>
2017-02-09 23:07:06 +0100

    la til vanlige kommandoer i unix

--- vanlige_git_kommandoer.sh
+++ vanlige_git_kommandoer.sh
@@ -6,6 +6,10 @@ echo 'COMMIT ALLE TRACKED FILES MED MESSAGE'
 echo "git commit -am '[message]'"
 echo ''
 
+echo 'PUSH TIL GIT REPOSITORY'
+echo 'git push'
+echo ''
+
 echo 'LAST NED ENDRINGER FRA GIT REPOSITORY'
 echo 'git pull'
 echo ''
