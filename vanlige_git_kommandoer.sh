fa2b2d08a1df2de9440bb670b7ce1b890c4c5e27
stanminister <stians_egen@hotmail.com>
2017-02-09 22:48:13 +0100

    La til vanlige git-kommandoer

---
+++ vanlige_git_kommandoer.sh
@@ -0,0 +1,15 @@
+echo 'LEGG TIL UNTRACKED FILES'
+echo 'git add -A'
+echo ''
+
+echo 'COMMIT ALLE TRACKED FILES MED MESSAGE'
+echo "git commit -am '[message]'"
+echo ''
+
+echo 'LAST NED ENDRINGER FRA GIT REPOSITORY'
+echo 'git pull'
+echo ''
+
+echo 'KLON REPOSITORY PÅ NYTT OM ALT GÅR ÅT HELVETE'
+echo 'git clone https://github.com/stanminister/Digipic.git'
+echo ''
